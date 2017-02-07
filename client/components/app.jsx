8ab883dc599db85fd3bf31a5fc7cd23a3b496605
humaid khan <humaid khan>
2017-02-07 22:30:45 +0400

    added click handler for clock designs

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,11 +3,18 @@ import Clock from './clock';
 import ClockList from './clockList';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.clockSelect = this.clockSelect.bind(this);
+  }
+  clockSelect(e){
+    console.log(e.target.className)
+  }
   render () {
     return (
             <div className="app">
             <Clock />
-            <ClockList />
+            <ClockList clockSelect={this.clockSelect}/>
             </div>);
   }
 }
--- client/components/clockList.jsx
+++ client/components/clockList.jsx
@@ -4,17 +4,15 @@ export default class CLockList extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentWillMount () {
-  }
   render () {
     return (
 
             <div className="clockList">
-            <div className="clockDesign d1">00:00:00</div>
-            <div className="clockDesign d2">00:00:00</div>
-            <div className="clockDesign d3">00:00:00</div>
-            <div className="clockDesign d4">00:00:00</div>
-            <div className="clockDesign d5">00:00:00</div>
+            <div className="clockDesign d1" onClick={this.props.clockSelect}>00:00:00</div>
+            <div className="clockDesign d2" onClick={this.props.clockSelect}>00:00:00</div>
+            <div className="clockDesign d3" onClick={this.props.clockSelect}>00:00:00</div>
+            <div className="clockDesign d4" onClick={this.props.clockSelect}>00:00:00</div>
+            <div className="clockDesign d5" onClick={this.props.clockSelect}>00:00:00</div>
             </div>
             );
   }
