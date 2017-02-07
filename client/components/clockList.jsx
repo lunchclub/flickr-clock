785deefabee655990f55be926f3e833e580f43ec
humaid khan <humaid khan>
2017-02-08 00:01:10 +0400

    fix small margin issue

--- client/components/clockList.jsx
+++ client/components/clockList.jsx
@@ -8,7 +8,7 @@ export default class CLockList extends React.Component {
     return (
 
             <div className="clockList">
-            <div className="clockDesign d1" onClick={this.props.clockSelect}>00:00:00</div>
+            <div className="clockDesign d1 m1" onClick={this.props.clockSelect}>00:00:00</div>
             <div className="clockDesign d2" onClick={this.props.clockSelect}>00:00:00</div>
             <div className="clockDesign d3" onClick={this.props.clockSelect}>00:00:00</div>
             <div className="clockDesign d4" onClick={this.props.clockSelect}>00:00:00</div>
